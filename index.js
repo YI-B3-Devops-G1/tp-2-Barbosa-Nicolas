b32b2b7add8044112131acb8c91ae8e6fc6b602f
NicolasB <nicolas.barbosa@volvo.com>
2020-02-03 14:36:15 +0100

    error resolved

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const app = express();
 
-const path = __dirname + '/views/';
 const port = 8080;
 
 app.get('/', function(req,res){
-    res.json("hello world");
+    res.json({message:"Hello World"});
   });
   
-  /*router.get('/status', function(req,res){
-    res.sendFile();
-  });*/
+app.get('/status', function(req,res){
+    res.json({
+        "status": "OK",
+        "postgresUptime": String,
+        "redisConnectedClients": Number
+    });
+  });
 
 
   app.listen(port)
